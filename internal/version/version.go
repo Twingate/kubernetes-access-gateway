cc7dae921ce4544c9f707b127f9a888f28f30af7
Minh Tu Le <minhtu@twingate.com>
2025-06-02 18:39:42 -0700

    Remove Commit and Date ldflags

--- internal/version/version.go
+++ internal/version/version.go
@@ -3,10 +3,4 @@ package version
 var (
 	// Version is the current version of the application.
 	Version = "dev"
-
-	// Commit is the git commit SHA at build time.
-	Commit = "unknown"
-
-	// Date is the build timestamp.
-	Date = "unknown"
 )
