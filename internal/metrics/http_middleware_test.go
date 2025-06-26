815fb1bbd69908a53c93b53ecdc3010e1b132ccd
Clement Tee <ctee@twingate.com>
2025-06-26 17:56:25 +0800

    better test

--- internal/metrics/http_middleware_test.go
+++ internal/metrics/http_middleware_test.go
@@ -6,119 +6,131 @@ import (
 	"testing"
 
 	"github.com/prometheus/client_golang/prometheus"
+	dto "github.com/prometheus/client_model/go"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestHTTPMetricsMiddleware(t *testing.T) {
-	testRegistry := prometheus.NewRegistry()
-
-	server := httptest.NewServer(HTTPMetricsMiddleware(testRegistry,
-		http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
-			w.WriteHeader(http.StatusOK)
-		}),
-	))
-	defer server.Close()
-
-	getReq, err := http.NewRequest(http.MethodGet, server.URL, nil)
-	require.NoError(t, err, "Failed to create request")
-
-	// Make a dummy GET request to trigger the middleware
-	getResp, err := http.DefaultClient.Do(getReq)
-	require.NoError(t, err, "failed to send request")
-	defer getResp.Body.Close()
-
-	metricFamilies, err := testRegistry.Gather()
-	require.NoError(t, err)
-
-	registeredLabels := make(map[string]map[string]string, len(metricFamilies))
-
-	for _, mf := range metricFamilies {
-		labels := mf.GetMetric()[0].GetLabel()
-		for _, label := range labels {
-			val, ok := registeredLabels[mf.GetName()]
-			if !ok {
-				val = make(map[string]string)
-			}
-
-			val[label.GetName()] = label.GetValue()
-			registeredLabels[mf.GetName()] = val
-		}
+	testCases := []struct {
+		name           string
+		setupRequest   func(*http.Request)
+		expectedLabels map[string]map[string]string
+	}{
+		{
+			name:         "HTTP Request",
+			setupRequest: func(req *http.Request) {}, // No special headers for regular HTTP
+			expectedLabels: map[string]map[string]string{
+				"twingate_gateway_http_requests_total": {
+					"method": "get",
+					"code":   "200",
+				},
+				"twingate_gateway_http_request_size_bytes": {
+					"type":   "http",
+					"method": "get",
+					"code":   "200",
+				},
+				"twingate_gateway_http_response_size_bytes": {
+					"type":   "http",
+					"method": "get",
+					"code":   "200",
+				},
+			},
+		},
+		{
+			name: "WebSocket Request",
+			setupRequest: func(req *http.Request) {
+				req.Header.Set("Upgrade", "websocket")
+				req.Header.Set("Connection", "upgrade")
+				req.Header.Set("Sec-WebSocket-Protocol", "v5.channel.k8s.io")
+			},
+			expectedLabels: map[string]map[string]string{
+				"twingate_gateway_http_requests_total": {
+					"method": "get",
+					"code":   "200",
+				},
+				"twingate_gateway_http_request_size_bytes": {
+					"type":   "websocket",
+					"method": "get",
+					"code":   "200",
+				},
+				"twingate_gateway_http_response_size_bytes": {
+					"type":   "websocket",
+					"method": "get",
+					"code":   "200",
+				},
+			},
+		},
+		{
+			name: "SPDY Request",
+			setupRequest: func(req *http.Request) {
+				req.Header.Set("Upgrade", "websocket")
+				req.Header.Set("Connection", "upgrade")
+				req.Header.Set("Sec-WebSocket-Protocol", "SPDY/3.1+portforward.k8s.io")
+			},
+			expectedLabels: map[string]map[string]string{
+				"twingate_gateway_http_requests_total": {
+					"method": "get",
+					"code":   "200",
+				},
+				"twingate_gateway_http_request_size_bytes": {
+					"type":   "spdy",
+					"method": "get",
+					"code":   "200",
+				},
+				"twingate_gateway_http_response_size_bytes": {
+					"type":   "spdy",
+					"method": "get",
+					"code":   "200",
+				},
+			},
+		},
 	}
 
-	assert.Equal(t, map[string]map[string]string{
-		"twingate_gateway_http_requests_total": {
-			"method": "get",
-			"code":   "200",
-		},
-		"twingate_gateway_http_request_size_bytes": {
-			"type":   "rest",
-			"method": "get",
-			"code":   "200",
-		},
-		"twingate_gateway_http_response_size_bytes": {
-			"type":   "rest",
-			"method": "get",
-			"code":   "200",
-		},
-	}, registeredLabels)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			testRegistry := prometheus.NewRegistry()
+
+			server := httptest.NewServer(HTTPMetricsMiddleware(testRegistry,
+				http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+					w.WriteHeader(http.StatusOK)
+				}),
+			))
+			defer server.Close()
+
+			req, err := http.NewRequest(http.MethodGet, server.URL, nil)
+			require.NoError(t, err, "failed to create request")
+
+			tc.setupRequest(req)
+
+			resp, err := http.DefaultClient.Do(req)
+			require.NoError(t, err, "failed to send request")
+			defer resp.Body.Close()
+
+			metricFamilies, err := testRegistry.Gather()
+			require.NoError(t, err)
+
+			registeredLabels := extractLabelsFromMetrics(metricFamilies)
+			assert.Equal(t, tc.expectedLabels, registeredLabels)
+		})
+	}
 }
 
-func TestHTTPMetricsMiddleware_WebSocketRequest(t *testing.T) {
-	testRegistry := prometheus.NewRegistry()
-
-	server := httptest.NewServer(
-		HTTPMetricsMiddleware(testRegistry,
-			http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
-				w.WriteHeader(http.StatusOK)
-			}),
-		),
-	)
-	defer server.Close()
-
-	wsReq, err := http.NewRequest(http.MethodGet, server.URL, nil)
-	require.NoError(t, err, "failed to create websocket request")
-	wsReq.Header.Set("Upgrade", "websocket")
-	wsReq.Header.Set("Connection", "upgrade")
-	wsReq.Header.Set("Sec-WebSocket-Protocol", "SPDY/3.1+portforward.k8s.io")
-
-	// Make a dummy WebSocket upgrade request to trigger the middleware
-	wsResp, err := http.DefaultClient.Do(wsReq)
-	require.NoError(t, err, "failed to send websocket request")
-	defer wsResp.Body.Close()
-
-	metricFamilies, err := testRegistry.Gather()
-	require.NoError(t, err)
-
+func extractLabelsFromMetrics(metricFamilies []*dto.MetricFamily) map[string]map[string]string {
 	registeredLabels := make(map[string]map[string]string, len(metricFamilies))
 
 	for _, mf := range metricFamilies {
 		labels := mf.GetMetric()[0].GetLabel()
 		for _, label := range labels {
-			val, ok := registeredLabels[mf.GetName()]
+			value, ok := registeredLabels[mf.GetName()]
 			if !ok {
-				val = make(map[string]string)
+				value = make(map[string]string)
 			}
 
-			val[label.GetName()] = label.GetValue()
-			registeredLabels[mf.GetName()] = val
+			value[label.GetName()] = label.GetValue()
+			registeredLabels[mf.GetName()] = value
 		}
 	}
 
-	assert.Equal(t, map[string]map[string]string{
-		"twingate_gateway_http_requests_total": {
-			"method": "get",
-			"code":   "200",
-		},
-		"twingate_gateway_http_request_size_bytes": {
-			"type":   "streaming",
-			"method": "get",
-			"code":   "200",
-		},
-		"twingate_gateway_http_response_size_bytes": {
-			"type":   "streaming",
-			"method": "get",
-			"code":   "200",
-		},
-	}, registeredLabels)
+	return registeredLabels
 }
