cdb2dcba16ccbf31727725946f81b41e7877a428
Clement Tee <ctee@twingate.com>
2026-04-06 16:00:00 +0800

    Update vault server docker command

--- test/integration/testutil/vault.go
+++ test/integration/testutil/vault.go
@@ -22,13 +22,15 @@ func SetupVaultServer(t *testing.T) (string, int) {
 
 	// #nosec G204 -- inputs are from trusted operator configuration
 	_, err := RunCommand(exec.Command("docker", "run", "-d",
+		"--cap-add=IPC_LOCK",
 		"-p", "0:8200",
 		"--name", containerName,
 		"-e", "VAULT_DEV_ROOT_TOKEN_ID=root",
+		"-e", "VAULT_DEV_LISTEN_ADDRESS=0.0.0.0:8200",
 		"-e", "VAULT_TOKEN=root",
 		"-e", "VAULT_ADDR=http://127.0.0.1:8200",
 		"hashicorp/vault:1.21.4",
-		"server", "-dev", "-dev-listen-address=0.0.0.0:8200",
+		"server", "-dev",
 	))
 	require.NoError(t, err, "failed to create Vault container")
 
